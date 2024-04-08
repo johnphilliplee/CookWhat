5dbc6df2a16275764b26c92ef5aa91178eea48d2
John Lee <johnlee.programming@gmail.com>
2024-04-08 11:28:03 +0800

    First iteration of theUI

--- CookWhat/CookWhatApp.swift
+++ CookWhat/CookWhatApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct CookWhatApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            IngredientInputView(recipeService: MockRecipeService())
         }
     }
 }
