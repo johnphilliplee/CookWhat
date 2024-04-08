c25bab81a620a32061a928f68f1e1576b4b417ee
John Lee <johnlee.programming@gmail.com>
2024-04-08 10:04:03 +0800

    Initial Commit

---
+++ CookWhat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CookWhat
+//
+//  Created by John Lee on 4/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CookWhat/CookWhatApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookWhatApp.swift
+//  CookWhat
+//
+//  Created by John Lee on 4/8/24.
+//
+
+import SwiftUI
+
+@main
+struct CookWhatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
